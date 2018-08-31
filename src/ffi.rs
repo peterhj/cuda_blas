f1f88a426fcc16bdbfe6d708a30936300ba931e0
Peter Jin <peterhaijin@gmail.com>
2018-08-31 13:49:07 -0700

    Use new FFI type paths.

--- src/ffi.rs
+++ src/ffi.rs
@@ -2,7 +2,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 
+use cuda::ffi::driver_types::{cudaStream_t};
 use cuda::ffi::library_types::{cudaDataType};
-use cuda::ffi::runtime::{cudaStream_t};
 
 include!(concat!(env!("OUT_DIR"), "/cublas_bind.rs"));
